ff8cd2f55d3b0554febae9f37b7cb2c3112559c1
ganiular <ganiular@gmail.com>
2023-11-23 23:38:07 +0100

    added sqrt_of+number function

--- sqrt_calculator.py
+++ sqrt_calculator.py
@@ -1,2 +1,11 @@
+import math
+
 def square_of_number(number):
     return number ** 2
+
+def sqrt_of_number(number):
+    return math.sqrt(number)
+
+
+if __name__ == "__main__":
+    print(sqrt_of_number(81))
