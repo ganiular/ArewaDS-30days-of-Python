90f9b25c9eff06996c3e4e0302e6525fb386290e
ganiular <ganiular@gmail.com>
2023-11-30 03:04:43 +0100

    added day1 exercise

---
+++ day1/helloworld.py
@@ -0,0 +1,51 @@
+## 1. Check the python version you are using
+# $ python --version
+
+
+## 2. Open the python interactive shell and do the following operations. The operands are 3 and 4.
+# addition
+print(3 + 4)
+
+# subtraction
+print(3 - 4)
+
+# multiplication
+print(3 * 4)
+
+# modulus
+print(3 % 4)
+
+# division
+print(3 / 4)
+
+# exponecial
+print(3 ** 4)
+
+# floor division
+print(3 // 4)
+
+
+## 3. Write strings on the python interactive shell. The strings are the following:
+# Your name
+print("Ganiu Olawale")
+
+# Your family name
+print("Quadri")
+
+# Your country
+print("Nigeria")
+
+print("I am enjoying 30 days of python")
+
+
+## 4. Check the data types of the following data:
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type("Ganiu Olawale"))
+print(type("Quadri"))
+print(type("Nigeria"))
+
+
